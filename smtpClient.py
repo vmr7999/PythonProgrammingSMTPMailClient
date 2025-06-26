ee59b55a9a2567aa374d7679eb0a9583aa51d895
vmr7999 <vmr7999@nyu.edu>
2025-06-26 17:24:32 -0400

    smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -72,6 +72,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     recv6 = clientSocket.recv(1024).decode()
     # Fill in end
 
+    clientSocket.close()
 
 if __name__ == '__main__':
     smtp_client(1025, '127.0.0.1')
